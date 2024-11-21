d6d8c009e7aebb91889e81956cede3e7c9384206
Git-hit <ayanali8178@gmail.com>
2024-11-21 21:33:34 +0530

    Added Popup and fixed techStack name

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -18,7 +18,7 @@ export default function Home(){
             <WhoWeAre />
             <Features />
             <TabNavs />
-            <TechStack />
+            {/* <TechStack /> */}
         </>
     )
 }
